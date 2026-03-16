381b0b0d1255b689740315fc93108eec42c0ff8b
pbhatt009 <bhattpriyanshu49@gmail.com>
2026-03-17 00:48:48 +0530

    Update server configuration to use environment variable for port and allow external access

--- main.py
+++ main.py
@@ -302,8 +302,13 @@ async def get_messages(thread_id: int):
 
     
 if(__name__ == "__main__"):
-    uvicorn.run("main:app", host="127.0.0.1", port=8000,reload=True)
-    # asyncio.run(register({"user":"hhfhfhf","password":"jfhjfjfj"}))
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port
+    )
  
       
     
